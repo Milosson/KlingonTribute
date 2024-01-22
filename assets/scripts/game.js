d0514c109ca8a753988e6af21539804f0287f993
Milosson <148342622+Milosson@users.noreply.github.com>
2024-01-22 21:14:19 +0000

    Add style to css for button-divs, and correcting some comments and misspellings.

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -1,11 +1,11 @@
 // Create an array with possbile choices 
 let choices = ["rock", "paper", "scissors", "spock", "lizard"];
 
-// Rules for the game -
+// Rules for the game - This way of object mapping was inspired by Hackernoon.com and Medium.com!
 let rules = {
     rock: { scissors: "crushes", lizard: "crushes" },
     paper: { rock: "covers", spock: "disapproves" },
     scissors: { paper: "cuts", lizard: "decapitates" },
-    spock: { rock: "vaporiz", scissors: "smashes" },
+    spock: { rock: "vaporizes", scissors: "smashes" },
     lizard: { paper: "eats", spock: "poisons" },
 }
