a7fcd7a417bfa2c9e355beee2bf453617326931a
Milosson <148342622+Milosson@users.noreply.github.com>
2024-01-23 10:03:46 +0000

    Add variables for scoretracking and object for emojiMap

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -9,3 +9,22 @@ let rules = {
     spock: { rock: "vaporizes", scissors: "smashes" },
     lizard: { paper: "eats", spock: "poisons" },
 }
+
+// Variable to keep track of total rounds. 
+let totalRounds = 0;
+
+// Variable to keep track wheter the game is over or not.
+let gameOver = false;
+
+// Variables for scoreboard both for user and computer.
+let playerScore = 0;
+let computerScore = 0;
+
+// Map for choices to emojis - This emoji map was inspired by Hackernoon.com! 
+const emojiMap = {
+    rock: '\uD83D\uDDFF', // 🗿 
+    paper: '\uD83D\uDCC3', // 📃
+    scissors: '\u2702\uFE0F', // ✂️
+    spock: '\uD83D\uDC7D', // 👽  
+    lizard: '\uD83E\uDD8E', // 🦎
+}
